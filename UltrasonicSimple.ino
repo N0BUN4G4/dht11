92de21dd27fc3866c18912bebfb250244fb654a5
N0BUN4G4 <57697115+N0BUN4G4@users.noreply.github.com>
2023-07-07 15:08:00 +0800

    Update UltrasonicSimple.ino

--- UltrasonicSimple.ino
+++ UltrasonicSimple.ino
@@ -1,22 +1,36 @@
-#include <Ultrasonic.h>
-
-/*
- * Pass as a parameter the trigger and echo pin, respectively,
- * or only the signal pin (for sensors 3 pins), like:
- * Ultrasonic ultrasonic(13);
- */
-Ultrasonic ultrasonic(8, 7);
+//ultrasonic pin
+int echoPin = 6;
+int trigPin = 7;
+long duration;
 int distance;
 
-void setup() {
+void setup()
+{
+  pinMode(echoPin, INPUT); //echo
+  pinMode(trigPin, OUTPUT); //trig
   Serial.begin(9600);
 }
 
-void loop() {
-  // Pass INC as a parameter to get the distance in inches
-  distance = ultrasonic.read();
-  
-  Serial.print("Distance in CM: ");
+void ultrasonic() //ultrasonic
+{
+  delay(1000); // 2-second delay
+  digitalWrite(trigPin, LOW); //ultrasonic trig pin
+  delayMicroseconds(2);
+
+  //ultrasonic trig pin
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  //ultrasonic echo pin
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+}
+
+void loop()
+{
+  ultrasonic(); //ultrasonic
+  Serial.print("Distance between Ultrasonic and Obstacle: ");
   Serial.println(distance);
-  delay(1000);
+  
 }
